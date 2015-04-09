21c42ebad996f120f7ab23680b619c42b42bfb74
jiangzhen <410096427@qq.com>
2015-04-09 21:22:35 +0800

    add basedbcontext

---
+++ BLL/BaseDbContext.cs
@@ -0,0 +1,68 @@
+﻿//#define sqlserver
+//#define oracle
+#define mysql
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Models;
+using System.Data.Entity.Infrastructure;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data.Entity.Core.Objects;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using CodeFirstStoreFunctions;
+
+namespace BLL
+{
+#if mysql
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+#endif
+    public class BaseDbContext : DbContext
+    {
+        /// <summary>
+        /// if not exists then The target context 'BLL.ASPDBContext' is not constructible. Add a default constructor or provide an implementation of IDbContextFactory.
+        /// </summary>
+        public BaseDbContext()
+        {
+            //Database.SetInitializer(new Initializer.SchoolContextInitializer_Oracle());
+            // Database.SetInitializer<SchoolContext>(null);
+        }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //一律使用架构名
+#if !mysql
+            //mysql 不需要设置 应该是连接字符串中已经有了
+            modelBuilder.HasDefaultSchema("SCHOOL");
+#endif
+#if oracle
+            //所有列明大写
+            modelBuilder.Properties()
+                .Configure(config => config.HasColumnName(config.ClrPropertyInfo.Name.ToUpper()));
+            //所有表名大写
+            modelBuilder.Types()
+                .Configure(config => config.ToTable(config.ClrType.Name.ToUpper()));
+#endif
+#if sqlserver || mysql
+            modelBuilder.Properties()
+                .Configure(config => config.HasColumnName(config.ClrPropertyInfo.Name));
+            modelBuilder.Types()
+                .Configure(config => config.ToTable(config.ClrType.Name));
+#endif
+            base.OnModelCreating(modelBuilder);
+        }
+        protected IObjectContextAdapter ObjectContextAdapter
+        {
+            get
+            {
+                return (IObjectContextAdapter)this;
+            }
+        }
+
+    }
+}
